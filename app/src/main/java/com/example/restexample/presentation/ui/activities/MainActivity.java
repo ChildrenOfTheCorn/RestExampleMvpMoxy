657872e0107fabc14ead6ae3eb1e33a84710f9ed
grishberg <grishberg@gmail.com>
2016-11-12 21:08:55 +0700

    fixed dependency to moxy with jack

--- app/src/main/java/com/example/restexample/presentation/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/restexample/presentation/ui/activities/MainActivity.java
@@ -3,18 +3,21 @@ package com.example.restexample.presentation.ui.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.example.restexample.R;
 import com.example.restexample.presentation.presenter.AuthorizationPresenter;
 import com.example.restexample.presentation.view.AuthorizationView;
 
-public class MainActivity extends AppCompatActivity implements AuthorizationView{
+public class MainActivity extends AppCompatActivity implements AuthorizationView {
+
+    //@InjectPresenter
+    AuthorizationPresenter presenter;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public void showProgress() {

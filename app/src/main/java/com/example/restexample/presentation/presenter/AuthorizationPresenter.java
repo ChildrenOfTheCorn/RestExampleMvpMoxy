63937beffc9b49f78243c5648f35130a950ee5b4
grishberg <grishberg@gmail.com>
2016-11-12 21:46:06 +0700

    fixed dependency to moxy without jack

--- app/src/main/java/com/example/restexample/presentation/presenter/AuthorizationPresenter.java
+++ app/src/main/java/com/example/restexample/presentation/presenter/AuthorizationPresenter.java
@@ -1,11 +1,13 @@
 package com.example.restexample.presentation.presenter;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 import com.example.restexample.presentation.view.AuthorizationView;
 
 /**
  * Created by grishberg on 05.11.16.
  */
+@InjectViewState
 public class AuthorizationPresenter extends MvpPresenter<AuthorizationView> {
     private static final String TAG = AuthorizationPresenter.class.getSimpleName();
 }
--- app/src/main/java/com/example/restexample/presentation/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/restexample/presentation/ui/activities/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.restexample.presentation.ui.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.example.restexample.R;
 import com.example.restexample.presentation.presenter.AuthorizationPresenter;
 import com.example.restexample.presentation.view.AuthorizationView;
 
 public class MainActivity extends AppCompatActivity implements AuthorizationView{
+
+    @InjectPresenter
+    AuthorizationPresenter presenter;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
